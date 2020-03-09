9a2a1d71fb816c579f9c78fee166680ad0b806e3
NishantUpadhyay-BTC <nishantupadhyay@botreetechnologies.com>
2020-03-09 12:59:34 +0530

    added model changes

--- download_models.sh
+++ download_models.sh
@@ -3,18 +3,21 @@
 echo "Downloading config files..."
 
 mkdir cfg
-wget -O cfg/coco.data https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/coco.data
-wget -O cfg/yolov3.cfg https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+wget -O cfg/coco.data https://public-model.s3-ap-southeast-1.amazonaws.com/coco.data
+wget -O cfg/yolov3-data.cfg https://public-model.s3-ap-southeast-1.amazonaws.com/yolov3-data.cfg
+wget -O cfg/voc.data https://public-model.s3-ap-southeast-1.amazonaws.com/voc.data
+wget -O cfg/voc.names https://public-model.s3-ap-southeast-1.amazonaws.com/voc.names
 
 echo "Modify config parameters to enable Testing mode"
-sed -i '/batch=64/c\# batch=64' cfg/yolov3.cfg
-sed -i '/subdivisions=16/c\# subdivisions=16' cfg/yolov3.cfg
-sed -i '/# batch=1/c\batch=1' cfg/yolov3.cfg
-sed -i '/# subdivisions=1/c\subdivisions=1' cfg/yolov3.cfg
+sed -i '/batch=64/c\# batch=64' cfg/yolov3-data.cfg
+sed -i '/subdivisions=16/c\# subdivisions=16' cfg/yolov3-data.cfg
+sed -i '/# batch=1/c\batch=1' cfg/yolov3-data.cfg
+sed -i '/# subdivisions=1/c\subdivisions=1' cfg/yolov3-data.cfg
 
 mkdir data
 wget -O data/coco.names https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
 
 echo "Downloading yolov3 weights"
 mkdir weights
-wget -O weights/yolov3.weights https://pjreddie.com/media/files/yolov3.weights
+wget -O weights/yolov3-data_final_26.weights https://public-model.s3-ap-southeast-1.amazonaws.com/yolov3-data_final_26.weights
+wget -O weights/voc.names https://public-model.s3-ap-southeast-1.amazonaws.com/voc.names
